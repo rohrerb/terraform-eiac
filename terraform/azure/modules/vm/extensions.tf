0a970884177eeba02c700ff2b529059d02a94975
Brandon Rohrer <brrohrer@microsoft.com>
2020-02-18 22:10:26 -0500

    win_extension

--- terraform/azure/modules/vm/extensions.tf
+++ terraform/azure/modules/vm/extensions.tf
@@ -1,14 +1,14 @@
 
 resource "azurerm_virtual_machine_extension" "extension" {
   for_each = { for vm in azurerm_virtual_machine.vm : vm.name => vm
-    if local.enable_log_analytics
+    if local.enable_log_analytics 
   }
 
   name                 = "LogAnalyticsMonitoring"
   virtual_machine_id   = each.value.id
   publisher            = "Microsoft.EnterpriseCloud.Monitoring"
-  type                 = "OmsAgentForLinux"
-  type_handler_version = "1.12"
+  type                 = var.os_code == var.os_code_linux ? "OmsAgentForLinux" : "MicrosoftMonitoringAgent"
+  type_handler_version = var.os_code == var.os_code_linux ? "1.12" : "1.0"
 
   settings = jsonencode({ workspaceId = local.log_analytics_worspace_id })
 
